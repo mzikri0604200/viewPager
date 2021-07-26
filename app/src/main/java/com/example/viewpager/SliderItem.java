4f4dbf15ffeb48dcee75bb7729c22997da89972e
mzikri0604200 <muhzikri06@gmail.com>
2021-07-26 11:49:16 +0700

    first commit

---
+++ app/src/main/java/com/example/viewpager/SliderItem.java
@@ -0,0 +1,14 @@
+package com.example.viewpager;
+
+public class SliderItem {
+//    Model CLass
+    private int image;
+
+    SliderItem(int image) {
+        this.image = image;
+    }
+
+    public int getimage() {
+        return image;
+    }
+}
